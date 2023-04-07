b0dd6a3f44613a53b89c81b490df16c08589c8f2
FelipeIgansi <lipeigansi@gmail.com>
2023-04-07 18:45:25 -0300

    adaptacao para usar botoes

--- app/src/main/java/com/devmaster/calculadorasimples/MainActivity.kt
+++ app/src/main/java/com/devmaster/calculadorasimples/MainActivity.kt
@@ -2,24 +2,16 @@ package com.devmaster.calculadorasimples
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.devmaster.calculadorasimples.databinding.ActivityMainBinding
 
-private const val SOMA = "Soma"
-
-private const val SUBTRACAO = "Subtração"
-
-private const val MULTIPLICACAO = "Multiplicação"
-
-private const val DIVISAO = "Divisão"
-
-private const val SELECIONE = "Selecione uma operação"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var value1 = 0.0
+    private var value2 = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,52 +20,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        loadSpinner()
-
-        binding.buttonCalcular.setOnClickListener(this)
+        binding.buttonSoma.setOnClickListener(this)
+        binding.buttonSubtracao.setOnClickListener(this)
+        binding.buttonMultiplicacao.setOnClickListener(this)
+        binding.buttonDivisao.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_calcular) {
-            val operacao = binding.spinnerOperacoes.selectedItem
-            if (operacao != SELECIONE) {
-                val val1 = binding.editTextValue1.text.toString().toInt()
-                val val2 = binding.editTextValue2.text.toString().toInt()
-                if (val1.toString() == "" || val2.toString() == "") Toast.makeText(
-                    this,
-                    "Faltou informat valor",
-                    Toast.LENGTH_SHORT
-                ).show()
-                else {
-                    when (operacao) {
-                        SOMA -> {
-                            binding.textResultado.text = (val1 + val2).toString()
-                        }
-                        SUBTRACAO -> {
-                            if (val1 >= val2) binding.textResultado.text = (val1 - val2).toString()
-                            else binding.textResultado.text = (val2 - val1).toString()
-                        }
-                        MULTIPLICACAO -> {
-                            binding.textResultado.text = (val1 * val2).toString()
-                        }
-                        DIVISAO -> {
-                            if (val1 >= val2) binding.textResultado.text = (val1 / val2).toString()
-                            else binding.textResultado.text = (val2 / val1).toString()
-                        }
-                    }
+        if (binding.editTextValue1.text.toString() != "" || binding.editTextValue2.text.toString() != "") {
+            value1 = binding.editTextValue1.text.toString().toDouble()
+            value2 = binding.editTextValue2.text.toString().toDouble()
+
+            when (v.id) {
+                R.id.button_soma -> {
+                    binding.textResultado.text = (value1 + value2).toString()
                 }
-            } else {
-                Toast.makeText(this, "Selecione uma opeção", Toast.LENGTH_SHORT).show()
+                R.id.button_subtracao -> {
+                    if (value1 >= value2) binding.textResultado.text = (value1 - value2).toString()
+                    else binding.textResultado.text = (value2 - value1).toString()
+                }
+                R.id.button_multiplicacao -> {
+                    binding.textResultado.text = (value1 * value2).toString()
+                }
+                R.id.button_divisao -> {
+                    if (value1 >= value2) binding.textResultado.text = (value1 / value2).toString()
+                    else binding.textResultado.text = (value2 / value1).toString()
+                }
             }
-        }
-
+        } else Toast.makeText(this, "Informar valor", Toast.LENGTH_SHORT).show()
     }
-
-    private fun loadSpinner() {
-        val list = listOf(SELECIONE, SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO)
-        val adapetr = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
-        binding.spinnerOperacoes.adapter = adapetr
-    }
-
 }
